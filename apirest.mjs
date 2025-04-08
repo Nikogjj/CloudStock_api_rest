bd150d215bea9286fe233e79fb47c14f0e08b5d5
Niko <nikolaguemmoud@hotmail.fr>
2025-04-08 13:55:53 +0200

    first

---
+++ apirest.mjs
@@ -0,0 +1,113 @@
+import express from "express"
+import cors from "cors"
+import fileUpload from "express-fileupload"
+import mariadb from "mariadb";
+import fs from "fs/promises"
+import { DocumentsModel } from "./models/DocumentsModel.js";
+import { json } from "stream/consumers";
+
+const app = express()
+const port = 3400;
+
+app.use(cors());
+app.use(express.static("public"));
+app.use(fileUpload());
+app.use(express.json());
+
+app.post("/upload_documents",async (req,res)=>{
+    const body = req.body;
+    const url = req.files.url;
+    console.log(body.name);
+    console.log(body.owner);
+    console.log(url);
+
+    if (url==undefined) {
+        req.status(400).json({msg:"Pas de document envoyé"});
+        return;
+    }
+
+    const extensionFile = url.name.split(".")[1];
+    const fileName = url.name.split(".")[0];
+    const completeFileName = `${fileName}_${Date.now()}.${extensionFile}`;
+    
+    url.mv(`./public/${completeFileName}`);
+
+    const connection = await DocumentsModel.connection();
+    const result = await DocumentsModel.createDocuments(body.name,body.owner,completeFileName);
+
+    res.json({url:`http://localhost:3400/${completeFileName}`});
+})
+// app.use((req,res)=>res.status(400).json({msg:"La route n'existe pas"}));
+
+app.get("/get_document/:filename",async (req,res)=>{
+    // je m'attend a recevoir juste l'url du file pour pouvoir le select par son nom dans ma bdd
+    // const filename = req.body.filename;
+    console.log("okdqkdsq");
+    const filename = req.params.filename;
+
+    const buf = await fs.readFile(`./public/${filename}`)
+
+    // const connection = await DocumentsModel.connection();
+    // const result = await DocumentsModel.getDocument(filename);
+
+    res.send(buf);
+    
+})
+
+app.get("/get_all_documents/:owner", async (req,res)=>{
+    // j'attend un objet avec attribut : name(owner)
+    // const body = req.body;
+    // console.log(body);
+    const name_owner = req.params.owner;
+    console.log(name_owner);
+
+    const connection = await DocumentsModel.connection();
+    const result = await DocumentsModel.getAllDocuments(name_owner);
+
+    res.json(result);
+})
+
+
+
+app.delete("/delete_document", async (req,res)=>{
+    // j'attend un objet avec juste l'url du fichier a supprimer
+    const filename = req.body.filename;
+    const connection = await DocumentsModel.connection();
+    const result = await DocumentsModel.deleteSingleDocument(filename);
+    
+    fs.rm(`./public/${filename}`);
+
+    console.log("result dans mon main : ",result);
+    res.json({
+        affectedRow:result.affectedRow,
+        insertId : Number(result.insertId),
+        warning : result.warning
+    });
+})
+
+app.delete("/delete_all_documents",async(req,res)=>{
+    // j'attend juste le nom de l'owner pour pouvoir tout delete dans la bdd
+    console.log(req.body.owner); 
+    const connection = await DocumentsModel.connection();
+    const tab_documents = await DocumentsModel.getAllDocuments(req.body.owner);
+
+    tab_documents.forEach((document) => {
+        console.log(`./public/${document.url}`);
+        // console.log(document.url);
+        fs.rm(`./public/${document.url}`);
+    });
+    const result = await DocumentsModel.deleteAllDocuments(req.body.owner);
+    console.log(result);
+    res.json({
+        affectedRow:result.affectedRow,
+        insertId : Number(result.insertId),
+        warning : result.warning
+    });
+
+})
+
+
+
+app.listen(port,()=>{
+    console.log(`server listen on port ${port}`)
+})
---
+++ models/DocumentsModel.js
@@ -0,0 +1,73 @@
+import mariadb from "mariadb";
+
+export class DocumentsModel{
+    static connection_etat =null;
+
+    static async connection(){
+        if ((DocumentsModel.connection_etat!=null)==true) {
+            console.log("ouai ouai")
+            const test = await DocumentsModel.deconnection();
+        }
+        return mariadb.createConnection({
+            host : "0.0.0.0",
+            port : 3306,
+            user : "root",
+            password : "root",
+            database : "CloudStock"
+        })
+        .then(connection=>{
+            DocumentsModel.connection_etat=connection;
+            // console.log("ICI : ",connection);
+            // console.log("ICI : ",DocumentsModel.connection_etat);
+            return DocumentsModel.connection_etat;
+        })
+    }
+
+    static async createDocuments(name,owner,completeFileName){
+        // console.log(name,owner,completeFileName);
+        const results = await DocumentsModel.connection_etat.query(`INSERT INTO Documents (name,owner,url) VALUES ("${name}","${owner}","${completeFileName}");`)
+        .catch(error=>console.error("Error de createDocuments : ",error.sqlMessage));
+
+        return results;
+    }
+
+    static async getDocument(finalename){
+        const result = await DocumentsModel.connection_etat.query(`SELECT * FROM Documents WHERE url="${finalename}"`)
+        .catch(error=>console.error(error));
+
+        return result;
+    }
+
+    static async getAllDocuments(owner){
+        const result = await DocumentsModel.connection_etat.query(`SELECT * FROM Documents WHERE owner="${owner}";`)
+        .catch(error => console.error(error));
+        return result;
+    }
+
+    static async deleteSingleDocument(fileName){
+        const result = await DocumentsModel.connection_etat.query(`DELETE FROM Documents WHERE url ="${fileName}";`)
+        .catch(error=>{
+            console.error(error);
+        });
+        console.log("result : ",result);
+        return result;
+    }
+
+    static async deleteAllDocuments(owner_name){
+        const result = await DocumentsModel.connection_etat.query(`DELETE FROM Documents WHERE owner="${owner_name}";`)
+        .catch(error=>console.error(error));
+        return result;
+    }
+
+    static async modifyDocument(filename){
+        
+    }
+
+    static deconnection(){
+        return DocumentsModel.connection_etat.end()
+        .then(()=>{
+            DocumentsModel.connection_etat=null;
+            return 1;
+        });
+    }
+}
